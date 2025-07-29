d054aad080b03e08cfe3f787455db949ae51da18
Jairo <jairoglez1999@gmail.com>
2025-07-29 04:42:54 -0400

    added an img as a testimonials bg

--- components/Home/Sections/Testimonials.tsx
+++ components/Home/Sections/Testimonials.tsx
@@ -1,6 +1,7 @@
 import SectionTItle from "../SectionTItle";
 import TestimonialCarousel from "../TestimonialCarousel";
 import AnimatedButton from "@/components/ui/Button";
+import Image from "next/image";
 
 const Testimonials = () => {
   return (
@@ -8,10 +9,19 @@ const Testimonials = () => {
       id="testimonials"
       className="relative py-16 md:px-10 bg-[#0c0f22] text-white text-center flex flex-col items-center justify-center"
     >
+      <Image
+        src="/assets/parallax-2.jpg"
+        alt="A radio img as a background"
+        layout="fill"
+        priority
+        quality={100}
+        objectFit="cover"
+        className="absolute inset-0 z-0 opacity-70"
+      />
       <SectionTItle title="Testimonials" subtitle="What our clients say" />
-      <TestimonialCarousel/>
-        <AnimatedButton className="!z-10" text="Leer Más" />
+      <TestimonialCarousel />
+      <AnimatedButton className="!z-10" text="Leer Más" />
     </section>
   );
-}
+};
 export default Testimonials;
