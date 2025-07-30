eace6ba5b824df63f4f2c946a0b912833e47f02b
Mandy9943 <armandoc9943@gmail.com>
2025-07-30 20:15:30 -0300

    refactor(Gallery): remove unused AnimatedButton import and format SectionTitle for consistency

--- components/Home/Sections/Gallery.tsx
+++ components/Home/Sections/Gallery.tsx
@@ -1,4 +1,3 @@
-import AnimatedButton from "@/components/ui/Button";
 import LatestPhotos from "../LatestPhotos";
 import ParticlesBackground from "../ParticlesBg";
 import SectionTItle from "../SectionTItle";
@@ -10,10 +9,12 @@ const Gallery = () => {
       className="relative py-16 bg-[#0c0f22] text-white text-center  flex flex-col items-center justify-center"
     >
       <ParticlesBackground />
-      <SectionTItle title="Últimas Fotos" subtitle="Tu mejor cara en la pista" />
+      <SectionTItle
+        title="Últimas Fotos"
+        subtitle="Tu mejor cara en la pista"
+      />
 
       <LatestPhotos />
-      <AnimatedButton className="!z-10 mt-16" text="All Gallery" />
     </section>
   );
 };
