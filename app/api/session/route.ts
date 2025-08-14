ac8906e70dd8324bc997126e326bc7e83aee9369
Mandy9943 <armandoc9943@gmail.com>
2025-08-13 21:03:06 -0300

    .

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -1,6 +1,6 @@
 import { getLoggedInUser } from "@/app/actions/auth.action";
 
-export async function GET(request: Request) {
+export async function GET() {
   const user = await getLoggedInUser();
   if (!user) {
     return new Response("Unauthorized", { status: 401 });
